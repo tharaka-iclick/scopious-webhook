a7a6b80c5db7df82b5abdd8c8c7267108e5a5aa7
Tharaka Udawatta <tharaka@i-context.io>
2025-06-18 09:38:05 +0530

    git commit -m "changes"

--- functions/app.js
+++ functions/app.js
@@ -262,6 +262,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const router = express.Router();
+const serverless = require("serverless-http");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
